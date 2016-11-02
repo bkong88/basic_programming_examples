bdbb669fa501636a2e5ea14874ecfc2eea999259
bkong88 <benardkong@hotmail.com>
2016-11-03 01:46:27 +0900

    Pretty much finished
    Just need to add demon. Can optionally add randomizers and a run_away option while battling.

--- python3/LearnToCodeExamples/text_game.py
+++ python3/LearnToCodeExamples/text_game.py
@@ -12,37 +12,35 @@ def battle(player, opponent):
     print("You're battling a", opponent)
     while player.health > 0 and opponent.health > 0:
         action = None
-        while action not in ('attack', 'check_inventory', 'check_health'):
+        moveOptions = ['attack', 'check_inventory', 'check_health']
+        while action not in moveOptions:
+            # Add a "run_away" option
             action = input("""What would you like to do?
             attack
             check_inventory
             check_health\n""")
-            """
-                TODO:
-                If the action is "attack", call the player's attack function
-                on the opponent.
-                If the action is "check_inventory", call the player's
-                print_inventory function.
-                If the action is "check_health", call the player's print_health
-                function
-                If the command isn't one of these, tell the user that
-                the command is invalid and that they should try again.
-            """
             if action == "attack":
-            	player.attack(opponent)
+                player.attack(opponent)
             elif action == "check_inventory":
-            	player.print_inventory()
+                player.print_inventory()
             elif action == "check_health":
-            	player.print_health()
-            # TODO: See above!
+                player.print_health()
         opponent.attack(player)
     if opponent.health <= 0:
         # TODO: The player should loot the opponent
-        print('Congratulations! You slayed', opponent)
+        player.loot(opponent)
+        print('Congratulations! You slayed %s.' % (opponent))
     if player.health <= 0:
         # TODO: Print to the screen that the player died
-        from sys import exit
-        exit(0)
+        replay = input("%s has died! Would you like to play again? (y/n)\n" % (player))
+        while replay != "y" and replay != "n":
+            replay = input("Please input 'y' or 'n'\n")
+        if replay == "y":
+            main()
+        elif replay == "n":
+            print("Thank you for playing!")
+            from sys import exit
+            exit(0)
 
 
 class Creature():
@@ -54,33 +52,23 @@ class Creature():
 
     def take_damage(self, damage):
         calculated_damage = damage - self.armor
-        print(self, 'lost', calculated_damage, 'health points in damage')
+        print(self, 'lost', calculated_damage, 'health points in damage. - ', end="")
         self.health -= calculated_damage
 
     def attack(self, target):
-        """
-            Print that this creature is attacking the target.
-            Call the target's take_damage method, giving it this
-            creature's damage as a parameter
-        """
-        print("%s is attacking %s." %(self, target))
+        print("%s attacks %s! - " % (self, target), end="")
         target.take_damage(self.damage)
-        print("%s now has %s HP remaining." %(target, target.health))
-        # pass # TODO: Remove this and see above!
+        print("%s's HP: %s." % (target, target.health), end="\n\n")
 
 
 class Adventurer(Creature):
     def __init__(self):
         super(Adventurer, self).__init__(30, 5, 10)
         self.inventory = []
+        self.name = input("What is your name great adventurer?\n")
 
     def __str__(self):
-        """
-         TODO: When we try to print an Adventurer object, we should
-         see the text "Noble Adventurer"
-        """
-        # pass # TODO: Remove this line and see above!
-        return("Noble Adventurer")
+        return self.name
 
     def loot(self, dead_creature):
         # Add the dead creature's loot to the adventurer's inventory
@@ -89,32 +77,28 @@ class Adventurer(Creature):
             print that the player looted the item and add it to
             the inventory
         """
+        for item in dead_creature.loot:
+            item_type = ""
+            item_value = 0
+            for key, value in item.attributes.items():
+                item_type = key
+                item_value = value
+            print("You find a %s, it increases your %s by %s!" % (item.name, item_type, item_value))
+            self.inventory.append((item.name, item.attributes))
         self.update_stats()
 
     def update_stats(self):
         self.armor = 5
         self.damage = 10
-        for name, value, attributes in self.inventory:
-            """
-                TODO:
-                    If the key "armor" is in the item attributes,
-                    we should add the value to the Adventurer's
-                    armor property
-                    If the key "damage" is in the item attributes,
-                    we should add the value to the Adventurer's
-                    damage property
-            """
-            item = Item(name, value, attributes) #attributes should be a Dictionary of {attributes: value}, should change this to reflect that
-            # try doing attributes.get("armor") != None, which returns True, then self.damage += attributes.get("armor")
-            if item.attributes == "damage":
-            	self.damage += item.value
-            elif item.attributes == "armor":
-            	self.armor += item.value
+        for name, attributes in self.inventory:
+            if attributes.get("armor") is not None:
+                self.armor += attributes.get("armor")
+            elif attributes.get("damage") is not None:
+                self.damage += attributes.get("damage")
         print("You now have : " + str(self.armor) + " armor.\n" + "You now have : " + str(self.damage) + " damage.")
-            #continue # TODO: remove this and see above!
 
     def print_inventory(self):
-        print('The adventurer is currently carrying:')
+        print('%s is currently carrying:' % (self.name))
         if len(self.inventory) == 0:
             print("\tNothing")
         else:
@@ -122,11 +106,11 @@ class Adventurer(Creature):
                 print("\t", item)
 
     def print_health(self):
-        print('The adventurer currently has', self.health, 'health')
+        print('%s currently has %s health.' % (self.name, self.health))
 
 
 # TODO: A Monster should inherit from Creature
-class Monster:
+class Monster(Creature):
     def __init__(self, health, armor, damage, loot):
         super(Monster, self).__init__(health, armor, damage)
         self.loot = loot
@@ -137,11 +121,9 @@ class Dragon(Monster):
         sword_of_dragon_slaying = Item('Sword of Dragon Slaying', {
             'damage': 10
         })
-        """
-            TODO: create an item called "Helm of Glory" with 5 armor
-            and add it to the Dragon's loot (Hint, very similar to the
-            Sword of Dragon Slaying)
-        """
+        helm_of_glory = Item('Helm of Glory', {
+            'armor': 5
+        })
         super(Dragon, self).__init__(40, 0, 20, [sword_of_dragon_slaying, helm_of_glory])
 
     def __str__(self):
@@ -166,14 +148,13 @@ class Boar(Monster):
     A Demon should carry one item as loot. This item should be called
     the Heavenly Hammer of Hammertime and should provide 15 damage.
     Make sure the player fights the Demon.
-
     Can you adjust the player's stats such that it's possible to win
     the game but not guaranteed?
     (I haven't actually done the math. Seriously, is it possible?)
 """
 
 
-class Item:
+class Item():
     def __init__(self, name, attributes):
         self.name = name
         self.attributes = attributes
