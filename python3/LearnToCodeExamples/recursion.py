ea378911773fc914a52f0be21883b5466a51d5dc
bkong88 <benardkong@hotmail.com>
2016-11-22 15:14:46 +0900

    Update recursion.py

--- python3/LearnToCodeExamples/recursion.py
+++ python3/LearnToCodeExamples/recursion.py
@@ -27,11 +27,20 @@ print("divisor:", divisor)
     3) No disk may be placed on top of a smaller disk.
     With three disks, the puzzle can be solved in seven moves. The minimum number of moves required to solve a Tower of Hanoi puzzle is 2n - 1, where n is the number of disks.
 """
-A = [5, 4, 3, 2, 1]
+def Hanoi(disks, source, dest, aux):
+  disks -= 1
+  if disks == 0:
+    dest.append(source.pop())
+  else:
+    Hanoi(disks, source, aux, dest)
+    dest.append(source.pop())
+    Hanoi(disks, aux, dest, source)
+
+A = [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 B = []
 C = []
-num_steps = hanoi(A, B, C)
-print("num_steps:", num_steps)
+
+Hanoi(len(A), A, B, C)
 
 # TODO: CHALLENGE: Create a binary search function called bsearch. Binary search takes in a sorted collection and keeps cutting it in half until if finds the desired value. It should take in an array and the value being searched for.
 collection = [17, 24, 39, 40, 143, 145, 171, 192, 196, 253, 333, 372, 584, 602, 632, 635, 763, 882, 891, 934, 999, 1009, 1131, 1140, 1154, 1222, 1257, 1376, 1408, 1422, 1574, 1647, 1668, 1732, 1889, 1936, 2003, 2023, 2185, 2191, 2207, 2227, 2279, 2284, 2289, 2419, 2448, 2512, 2530, 2539, 2546, 2643, 2647, 2650, 2663, 2676, 2800, 2822, 2838, 2843, 2948, 2980, 2991, 2993, 3000, 3163, 3190, 3202, 3253, 3276, 3281, 3296, 3393, 3474, 3533, 3552, 3576, 3681, 3722, 3791, 3800, 3828, 3849, 3869, 3872, 3939, 3964, 3992, 4016, 4186, 4229, 4232, 4349, 4520, 4541, 4619, 4741, 4856, 4861, 4958]
